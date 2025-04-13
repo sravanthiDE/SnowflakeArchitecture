33c59a989a08392296cfc0ad79bf63a18a191cce
sravanthiDE <sravanthi_veep@yahoo.com>
2025-04-13 12:13:36 -0700

    created new files

---
+++ warehouse_script.sql
@@ -0,0 +1,18 @@
+select * from SNOWFLAKE_SAMPLE_DATA.TPCH_SF10.CUSTOMER limit 100;  8 AM  -- 2 minutes --1 minute
+
+select * from SNOWFLAKE_SAMPLE_DATA.TPCH_SF10.NATION limit 1000; -- 8.03 AM  -- 8.04 AM  -- 8.05 AM
+
+ALTER WAREHOUSE REPORTING_WH RESUME   -- 8 AM
+ALTER WAREHOUSE REPORTING_WH SUSPEND  -- 70 seconds   -- 70 seconds 
+
+8 AM
+
+1 minute  -- 8.01 
+
+5 minutes  
+ALTER WAREHOUSE REPORTING_WH SUSPEND 
+6 minute
+
+8.02 AM -- 2 minutes
+
+show warehouses
